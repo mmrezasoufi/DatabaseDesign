fe1e49d4ed87c12ff6e5b5b075a96f9390763cba
Seyed Amir Hosseini <seyedamirhosseini00@gmail.com>
2024-01-06 15:47:03 +0330

    Fix some changes

--- projects/academic_staff_management_system/backend/api/serializers.py
+++ projects/academic_staff_management_system/backend/api/serializers.py
@@ -1,12 +1,93 @@
-class ResearcherSerializer(serializers.HyperlinkedModelSerializer):
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from . import models
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = models.Researcher
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_staff",
+            "is_active",
+            "is_superuser",
+            "last_login",
+            "date_joined",
+        ]
+
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Person
         fields = "__all__"
 
 
-class ResearchSerializer(serializers.HyperlinkedModelSerializer):
+class AddressSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = models.Research
+        model = models.Address
+        fields = "__all__"
+
+
+class EducationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Education
+        fields = "__all__"
+
+
+class PhoneNumberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.PhoneNumber
+        fields = "__all__"
+
+
+class BuildingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Building
+        fields = "__all__"
+
+
+class FacultySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Faculty
+        fields = "__all__"
+
+
+class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Department
+        fields = "__all__"
+
+
+class OfficeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Office
+        fields = "__all__"
+
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Employee
+        fields = "__all__"
+
+
+class FieldSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Field
+        fields = "__all__"
+
+
+class ProfessorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Professor
+        fields = "__all__"
+
+
+class ResearcherSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Researcher
         fields = "__all__"
 
 
--- projects/academic_staff_management_system/backend/api/views.py
+++ projects/academic_staff_management_system/backend/api/views.py
@@ -1,3 +1,82 @@
+from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from django.contrib.auth.models import User
+from . import serializers
+from . import models
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = models.Person.objects.all()
+    serializer_class = serializers.PersonSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = models.Address.objects.all()
+    serializer_class = serializers.AddressSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = models.Education.objects.all()
+    serializer_class = serializers.EducationSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class PhoneNumberViewSet(viewsets.ModelViewSet):
+    queryset = models.PhoneNumber.objects.all()
+    serializer_class = serializers.PhoneNumberSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class BuildingViewSet(viewsets.ModelViewSet):
+    queryset = models.Building.objects.all()
+    serializer_class = serializers.BuildingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class FacultyViewSet(viewsets.ModelViewSet):
+    queryset = models.Faculty.objects.all()
+    serializer_class = serializers.FacultySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = models.Department.objects.all()
+    serializer_class = serializers.DepartmentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class OfficeViewSet(viewsets.ModelViewSet):
+    queryset = models.Office.objects.all()
+    serializer_class = serializers.OfficeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = models.Employee.objects.all()
+    serializer_class = serializers.EmployeeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class FieldViewSet(viewsets.ModelViewSet):
+    queryset = models.Field.objects.all()
+    serializer_class = serializers.FieldSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = models.Professor.objects.all()
+    serializer_class = serializers.PersonSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
 class ResearcherViewSet(viewsets.ModelViewSet):
     queryset = models.Researcher.objects.all()
     serializer_class = serializers.ResearcherSerializer
