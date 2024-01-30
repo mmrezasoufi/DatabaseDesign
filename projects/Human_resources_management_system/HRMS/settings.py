3cd3a5135cd443aab6cdc73371d99f334e46274f
pashanaeim <pashanaeim@gmail.com>
2024-01-30 15:40:19 +0330

    auth app added

--- projects/Human_resources_management_system/HRMS/settings.py
+++ projects/Human_resources_management_system/HRMS/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'auth',
     'users',
     'personels',
     'resume',
--- projects/Human_resources_management_system/HRMS/urls.py
+++ projects/Human_resources_management_system/HRMS/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("auth/", include("auth.urls")),
     path("users/", include("users.urls")),
     path("personels/", include("personels.urls")),
     path("course/", include("course.urls")),
