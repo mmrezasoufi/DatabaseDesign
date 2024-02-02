7354116abfaa78135749c2352c23bb235a60ed07
ArmanLK <74207911+ArmanLK@users.noreply.github.com>
2024-02-03 00:08:22 +0330

    Added meeting

--- projects/Human_resources_management_system/interview_meeting/admin.py
+++ projects/Human_resources_management_system/interview_meeting/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Interview, Meeting, PersonelsMeeting
 
-# Register your models here.
+
+@admin.register(Interview)
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = ("Interviewer", "applier", "stage", "interview_date", "created")
+
+
+@admin.register(Meeting)
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = ("establisher", "title", "start_date", "created")
+
+
+@admin.register(PersonelsMeeting)
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = ("personel", "meeting", "created")
--- projects/Human_resources_management_system/interview_meeting/models.py
+++ projects/Human_resources_management_system/interview_meeting/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from personels.models import Personel
+from users.models import User
 
-# Create your models here.
+
+class Interview(models.Model):
+    interviewer = models.ForeignKey(
+        Personel, on_delete=models.CASCADE, related_name="interviews"
+    )
+    aplier = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="interviews"
+    )
+    stage = models.PositiveSmallIntegerField(default=0)
+    interview_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "interview"
+        verbose_name_plural = "interviews"
+
+    def __str__(self):
+        return f"inte:{self.interviewer} - appl:{self.aplier}"
+
+
+class Meeting(models.Model):
+    establisher = models.ForeignKey(
+        Personel, on_delete=models.CASCADE, related_name="meeting_owner"
+    )
+    title = models.CharField(max_length=45, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "meeting"
+        verbose_name_plural = "meetings"
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class PersonelsMeeting(models.Model):
+    personel = models.ForeignKey(
+        Personel, on_delete=models.CASCADE, related_name="has_meetings"
+    )
+    meeting = models.ForeignKey(
+        Meeting, on_delete=models.CASCADE, related_name="personels_meeting"
+    )
+
+    def __str__(self):
+        return f"{self.personel} - {self.meeting}"
