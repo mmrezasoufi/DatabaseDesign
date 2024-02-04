a7896097b81cbe96eca4b0e5242b882cd41251f1
mmrezasoufi <mrsoofi9@gmail.com>
2024-02-04 19:27:59 +0330

    course views added

--- projects/Human_resources_management_system/course/urls.py
+++ projects/Human_resources_management_system/course/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import CourseList, PersonelCourseList
 
 
 urlpatterns = [
-
+    path("", CourseList.as_view(), name="course-list"),
+    path("personel/", PersonelCourseList.as_view(), name="personel-courses")
 ]
--- projects/Human_resources_management_system/course/views.py
+++ projects/Human_resources_management_system/course/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Course, PersonelCourse
+from .serializers import CourseSerializer, PersonelCourseSerializer
+from utils import CustomPagination
 
-# Create your views here.
+
+class CourseList(ListAPIView):
+    serializer_class = CourseSerializer
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        courses = Course.objects.select_related("instructor", "building").all()
+        return courses
+
+
+class PersonelCourseList(ListAPIView):
+    serializer_class = PersonelCourseSerializer
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        personel_courses = PersonelCourse.objects.select_related("course", "personel").all()
+        return personel_courses
