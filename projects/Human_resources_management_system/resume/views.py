ee8936b25fef93f0064644e43b43ac9a0e8c0e17
pashanaeim <pashanaeim@gmail.com>
2024-02-04 14:41:27 +0330

    Resume Views Added

--- projects/Human_resources_management_system/resume/views.py
+++ projects/Human_resources_management_system/resume/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Resume
+from .serializers import ResumeSerializer
+from utils import CustomPagination
 
-# Create your views here.
+
+class ResumeList(ListAPIView):
+    serializer_class = ResumeSerializer
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        resumes = Resume.objects.prefetch_related("degrees", "skills", "experiences").all()
+        return resumes
+    
+
+class ResumeDetail(APIView):
+
+    def get(self, request, username):
+
+        resume = get_object_or_404(Resume, user__username=username)
+        serializer = ResumeSerializer(resume)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
