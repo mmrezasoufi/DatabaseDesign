293853572d22c4f40aeee8a12dcf1d1d52f8327e
mmrezasoufi <mrsoofi9@gmail.com>
2024-02-04 03:34:48 +0330

    corse model added

--- projects/Human_resources_management_system/course/admin.py
+++ projects/Human_resources_management_system/course/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Course, PersonelCourse
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("instructor", "building", "title", "start_date", "end_date", "created")
+
+
+@admin.register(PersonelCourse)
+class PersonelCourseAdmin(admin.ModelAdmin):
+    list_display = ("personel", "course", "score", "created")
--- projects/Human_resources_management_system/course/models.py
+++ projects/Human_resources_management_system/course/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from personels.models import Personel
+from department.models import Building
 
-# Create your models here.
+
+class Course(models.Model):
+
+    instructor = models.ForeignKey(Personel, on_delete=models.CASCADE, related_name="courses")
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name="courses")
+    title = models.CharField(max_length=45, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "course"
+        verbose_name_plural = "courses"
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class PersonelCourse(models.Model):
+
+    personel = models.ForeignKey(Personel, on_delete=models.CASCADE, related_name="personel_courses")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="personel_courses")
+    score = models.PositiveSmallIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "course"
+        verbose_name_plural = "courses"
+
+    def __str__(self) -> str:
+        return f"{self.personel} - {self.course}"
+    
